f3702982003ed21d1c95df0c8d005a1185e1e4b0
Zhakha749 <z_shakarbekov@kbtu.kz>
2026-04-01 10:23:50 +0500

    Add Lab9 folder

---
+++ Lab9/shop-back/shop_back/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for shop_back project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_back.settings')
+
+application = get_wsgi_application()
